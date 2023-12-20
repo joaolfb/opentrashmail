948e50845f2aab7e5fdb1b5be19bb87c946d13ec
joaolfb <joaolfb@gmail.com>
2023-12-20 10:40:50 +0000

    update

--- web/templates/index.html.php
+++ web/templates/index.html.php
@@ -4,18 +4,17 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="/css/pico.min.css">
-  <link rel="stylesheet" href="/css/fontawesome.min.css">
-  <link rel="stylesheet" href="/css/prism.css">
-  <link rel="stylesheet" href="/css/opentrashmail.css">
+  <link rel="stylesheet" href="css/pico.min.css">
+  <link rel="stylesheet" href="css/fontawesome.min.css">
+  <link rel="stylesheet" href="css/prism.css">
+  <link rel="stylesheet" href="css/opentrashmail.css">
   <title>Open Trashmail</title>
 </head>
 
 <body>
   <div class="topnav" id="OTMTopnav">
-    <a href="/"><img src="/imgs/logo-50.png" width="50px" /> Home <small class="version"><?=getVersion()?></small></a>
-    <a><input id="email" hx-post="/api/address" hx-target="#main" name="email" type="email" style="margin-bottom:0px" hx-trigger="input changed delay:500ms" placeholder="email address" aria-label="email address"></a>
-    <a href="/random" hx-get="/api/random" hx-target="#main"><i class="fas fa-random"></i> Generate random</a>
+    <a href="/"><i class="fa fa-home" style="font-size:28px;"></i> Home <small class="version"><?=getVersion()?></small></a>
+    <a href="/random" hx-get="/api/random" hx-target="#main"><i class="fas fa-random"></i> Generate random Email</a>
     <?php if($this->settings['ADMIN_ENABLED']==true):?><a href="/admin" hx-get="/api/admin" hx-target="#main" hx-push-url="/admin"><i class="fas fa-user-shield"></i> Admin</a><?php endif; ?>
     <a href="javascript:void(0);" class="icon" onclick="navbarmanager()">
       <i class="fa fa-bars"></i>
